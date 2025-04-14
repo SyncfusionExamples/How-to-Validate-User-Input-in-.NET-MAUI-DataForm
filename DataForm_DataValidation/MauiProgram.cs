4e03af378adf34c5f2282db7dda9ecb1334357e7
SriRadheshNag <sriradheshnag.s@syncfusion.com>
2025-04-14 12:44:05 +0530

    Updated changes

--- DataForm_DataValidation/MauiProgram.cs
+++ DataForm_DataValidation/MauiProgram.cs
@@ -15,10 +15,12 @@ namespace DataForm_DataValidation
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "Roboto");
                 });
 
+
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
